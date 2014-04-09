9bf6125fa315bfd19f7ca39dfce1da00372a1dcd
tokhi <shafi.tokhi@gmail.com>
2014-04-09 18:31:42 +0200

    create option menu

--- src/com/example/flower/MainActivity.java
+++ src/com/example/flower/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.flower;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.example.tecktalk_android.R;
 
@@ -11,5 +12,12 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.flower_layout);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
 	}
 }
