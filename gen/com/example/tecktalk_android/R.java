e449c314a3c56afc8336cc280ee8e490de56dbfd
tokhi <shafi.tokhi@gmail.com>
2014-04-09 18:20:12 +0200

    large text view + basic dimensions

--- gen/com/example/tecktalk_android/R.java
+++ gen/com/example/tecktalk_android/R.java
@@ -21,20 +21,25 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int hibiscus=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int pimp_scarlet=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080007;
         public static final int button1=0x7f080001;
-        public static final int calendarView1=0x7f080004;
+        public static final int calendarView1=0x7f080006;
         public static final int editText1=0x7f080002;
+        public static final int imageView1=0x7f080004;
         public static final int layout=0x7f080003;
         public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int code_layout=0x7f030001;
-        public static final int linear_layout=0x7f030002;
+        public static final int flower_layout=0x7f030002;
+        public static final int linear_layout=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -43,7 +48,9 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int click_me=0x7f050005;
+        public static final int flower=0x7f050006;
         public static final int hello_world=0x7f050002;
+        public static final int instruction=0x7f050007;
         public static final int name_lable=0x7f050003;
         public static final int ok_bttn=0x7f050004;
     }
---
+++ src/com/example/flower/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.flower;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.tecktalk_android.R;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.flower_layout);
+	}
+}
