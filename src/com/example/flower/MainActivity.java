9b68ec242717fb647dd5cbc1c41221d8d3bc68c8
tokhi <shafi.tokhi@gmail.com>
2014-04-10 10:50:50 +0200

    playing with the log cat

--- src/com/example/flower/MainActivity.java
+++ src/com/example/flower/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.flower;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 
 import com.example.tecktalk_android.R;
@@ -12,12 +13,20 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.flower_layout);
+		
+		/**
+		 * Intentional error to test log cat
+		 */
+//		int[] testArray = new int[10];
+//        int number = testArray[11];
+        Log.d("MainActivity", "Activity created successfully!");
 	}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        Log.d("MainActivity", "On create option menu triggered!");
 		return super.onCreateOptionsMenu(menu);
 	}
 }
