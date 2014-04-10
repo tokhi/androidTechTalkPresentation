7a6329c6bc4a2e439c22f11a8a481f06bd2253a0
tokhi <shafi.tokhi@gmail.com>
2014-04-10 11:00:53 +0200

    Selecting a menu item

--- src/com/example/flower/MainActivity.java
+++ src/com/example/flower/MainActivity.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.tecktalk_android.R;
 
 public class MainActivity extends Activity {
+
+	private static final String LOG_TAG = "MainActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +23,20 @@ public class MainActivity extends Activity {
 		 */
 //		int[] testArray = new int[10];
 //        int number = testArray[11];
-        Log.d("MainActivity", "Activity created successfully!");
+        Log.d(LOG_TAG, "Activity created successfully!");
 	}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-        Log.d("MainActivity", "On create option menu triggered!");
+        Log.d(LOG_TAG, "On create option menu triggered!");
 		return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Toast.makeText(this, "You tapped the "+item.getTitle(), Toast.LENGTH_LONG).show();
+		return super.onOptionsItemSelected(item);
 	}
 }
