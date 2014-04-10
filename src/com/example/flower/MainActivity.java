f31fb1211ff81c32e1bfad671ea3427f2a24b0a7
tokhi <shafi.tokhi@gmail.com>
2014-04-10 13:55:14 +0200

    playing with activity life cycles

--- src/com/example/flower/MainActivity.java
+++ src/com/example/flower/MainActivity.java
@@ -57,4 +57,38 @@ public class MainActivity extends Activity {
 		startActivity(intent);
 
 	}
+	
+	/**
+	 * playing with activity life cycles
+	 */
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.d(LOG_TAG, "onResume");
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.d(LOG_TAG, "onStart");
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.d(LOG_TAG, "onPause");
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.d(LOG_TAG, "onStop");
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.d(LOG_TAG, "onDestroy");
+	}
 }
