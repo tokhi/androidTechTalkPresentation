89f434da36ad14f92c29113fad22c9e034ab4229
tokhi <shafi.tokhi@gmail.com>
2014-04-10 13:48:11 +0200

    back button to home

--- src/com/example/flower/DetailActivity.java
+++ src/com/example/flower/DetailActivity.java
@@ -4,6 +4,7 @@ import com.example.tecktalk_android.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class DetailActivity extends Activity{
 	
@@ -11,6 +12,17 @@ public class DetailActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_detail);
+		
+		// back button
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if(item.getItemId() == android.R.id.home){
+			finish();
+		}
+		return super.onOptionsItemSelected(item);
 	}
 	
 
